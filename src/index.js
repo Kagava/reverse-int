c09632c900eacfc89b1a7fd466071695eb48ccc1
Kagava <esspakov@mail.ru>
2024-07-30 19:24:08 +0700

    init: start tast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString(10);
+    let ans = '';
+    if (n < 0) {
+        for (let i = str.length - 1; i > 0; i--) {
+            ans = ans + str[i];
+        }
+    }
+    else {
+        for (let i = str.length - 1; i >= 0; i--) {
+            ans = ans + str[i];
+        }
+    }
+    
+    
+    return ans;
 }
+  
